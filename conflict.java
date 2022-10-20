dc1e1b6d6ef33bb13e1bd793bf5b8b2ed2255f33
UserIOI <paulharaburda@gmail.com>
2022-10-20 11:37:37 +0200

    my changes

--- conflict.java
+++ conflict.java
@@ -1,5 +1,7 @@
 public class conflict{
     public static void main(String[] args) {
         System.out.println("czesc");
+        System.out.println("czesc");
+        System.out.print("okej");
     }
 }
